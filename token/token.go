c8dc932cf3dbf5a8a8b3514130c4b5d9607624cd
haitammabrouk <haitammk0708@gmail.com>
2024-05-02 12:41:16 +0100

    define lookUpIdent function

--- token/token.go
+++ token/token.go
@@ -32,3 +32,16 @@ const (
 	FUNCTION = "FUNCTION"
 	LET = "LET"
 )
+
+var keywords = map[string]TokenType {
+	"fn" : FUNCTION,
+	"let" : LET,
+}
+
+// a function to check if the literal is either an identifier or a keyword
+func lookUpIdent(ident string) TokenType {
+	if tok, ok := keywords[ident] ; ok{
+		return tok
+	}
+	return IDENT
+}
